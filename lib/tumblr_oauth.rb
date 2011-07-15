a5de1f68488104fc57f8918ba5791dc4442dd821
John Bunting <codingjester@gmail.com>
2011-07-15 14:04:58 -0400

    Hoe to create the initial setup for the gem.

---
+++ lib/tumblr_oauth.rb
@@ -0,0 +1,3 @@
+class TumblrOauth
+  VERSION = '1.0.0'
+end
---
+++ test/test_tumblr_oauth.rb
@@ -0,0 +1,8 @@
+require "test/unit"
+require "tumblr_oauth"
+
+class TestTumblrOauth < Test::Unit::TestCase
+  def test_sanity
+    flunk "write tests or I will kneecap you"
+  end
+end
